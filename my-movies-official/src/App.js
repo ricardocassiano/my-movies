1fd725f5cf895fcb83ba3b85bdc8247bccaa7958
ricardocassiano <ricardocassianoti@gmail.com>
2021-04-16 01:00:58 -0300

    Criação do app oficial "my-movies-official" O "my-movies" será excluído posteriormente. Criação dos principais componentes da aplicação

---
+++ my-movies-official/src/App.js
@@ -0,0 +1,19 @@
+import { Header } from "./components/Header";
+import { MoviesList } from "./components/MoviesList";
+
+
+function App() {
+  return (
+    <div className="App">
+      <Header />
+
+      <section>
+        <MoviesList />
+      </section>
+    </div>
+
+    
+  );
+}
+
+export default App;
---
+++ my-movies-official/src/components/Header.js
@@ -0,0 +1,10 @@
+import { MainMenu } from "./MainMenu";
+
+export const Header = () => (
+    <header>
+        <h1>My Movies</h1>
+        
+        <MainMenu />
+    </header>
+    
+);
